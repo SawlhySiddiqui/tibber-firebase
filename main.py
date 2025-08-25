5b4fde6e80124bb10109e8d7690282b089c8bd93
ghaniya <ghaniya592@gmail.com>
2025-08-25 17:55:47 +0500

    Fix Tibber rt_subscribe TypeError by passing callback as keyword

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ async def run_tibber():
         home = tibber_connection.get_homes()[0]
 
         # Subscribe to real-time data
-        await home.rt_subscribe(_callback,loop=asyncio.get_event_loop())
+        await home.rt_subscribe(callback=_callback,loop=asyncio.get_event_loop())
 
         # Keep alive
         while True:
